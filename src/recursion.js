28908e08a49f35249ec371d2f265aee4dc3fc49a
Scott Hallock <scott.hallock@gmail.com>
2023-01-24 21:20:04 -0600

    solve problem 7

--- src/recursion.js
+++ src/recursion.js
@@ -65,6 +65,8 @@ var range = function(x, y) {
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+  if (exp === 0) return 1;
+  return exp > 0 ? base * exponent(base, exp - 1) : 1 / (base * exponent(base, -exp - 1));
 };
 
 // 8. Determine if a number is a power of two.
