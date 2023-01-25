83661f6b586230103f2a302485c1d72942e91015
Scott Hallock <scott.hallock@gmail.com>
2023-01-24 22:41:25 -0600

    solve problem 11

--- src/recursion.js
+++ src/recursion.js
@@ -99,6 +99,16 @@ var palindrome = function(string) {
 // modulo(17,5) // 2
 // modulo(22,6) // 4
 var modulo = function(x, y) {
+  if (y === 0) return NaN;
+  if ((x <= 0 && y < 0 && -x < -y) ||
+    (x <= 0 && y > 0 && -x < y) ||
+    (x >= 0 && y < 0 && x < -y) ||
+    (x >= 0 && y > 0 && x < y)) {
+    return x;
+  }
+
+  if ((x < 0 && y < 0) || (x > 0 && y > 0)) return modulo(x - y, y);
+  if ((x < 0 && y > 0) || (x > 0 && y < 0)) return modulo(x + y, y);
 };
 
 // 12. Write a function that multiplies two numbers without using the * operator or
